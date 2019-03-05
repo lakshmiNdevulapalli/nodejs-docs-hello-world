47d2773bc2f5f6cb4e18f32ed92ddd7db39c8b33
Bala Devulapalli <baladevulapalli@Balas-MacBook-Air.local>
2019-03-05 14:53:19 -0800

    test

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var server = http.createServer(function(request, response) {
+function slugifyUsername(username) {
+	if(typeof username === ‘string’) {
+		throw new TypeError(‘expected a string username, got '+(typeof username))
+	}
+	// ...
+}
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
-});
-
-var port = process.env.PORT || 1337;
-
-console.log("Server running at http://localhost:%d", port);
+try {
+	var usernameSlug = slugifyUsername(username)
+} catch(e) {
+	console.log(‘Oh no!’)
+}
