24ddd95ae88b3025e8f49028f9f2daf0a8de168d
Bala Devulapalli <baladevulapalli@Balas-MacBook-Air.local>
2019-03-05 14:56:32 -0800

    test

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 function slugifyUsername(username) {
 	if(typeof username === ‘string’) {
-		throw new TypeError(‘expected a string username, got '+(typeof username))
+		throw new TypeError(‘expected a string username, got '+(typeof username));
 	}
-	// ...
 }
 
 try {
 	var usernameSlug = slugifyUsername(username)
 } catch(e) {
-	console.log(‘Oh no!’)
+	console.log(‘Oh no!’);
 }
