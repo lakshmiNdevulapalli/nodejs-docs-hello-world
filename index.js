629f7b865eddd682eb061ef9caaddd0b5554a44d
Bala Devulapalli <baladevulapalli@Balas-MacBook-Air.local>
2019-03-05 15:01:41 -0800

    testing

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-function slugifyUsername(username) {
-	if(typeof username === ‘string’) {
-		throw new TypeError(‘expected a string username, got '+(typeof username));
-	}
-}
-
-try {
-	var usernameSlug = slugifyUsername(username)
-} catch(e) {
-	console.log(‘Oh no!’);
-}
+var promise1 = new Promise(function(resolve, reject) {
+	etTimeout(function() {
+	  resolve('foo');
+	}, 300);
+  });
